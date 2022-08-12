2c4e412a20aacdd28a18c9fd750554945ecd05d4
breabang <breanna.bang@gmail.com>
2022-08-12 17:01:34 -0600

    updating

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 
+
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'Views/index.ejs')
+    res.sendFile(__dirname + '/index.html')
     // Note: __dirname is the current directory you're in. Try logging it and see what you get!
     // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
   })
