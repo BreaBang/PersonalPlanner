cd49d22cc5e3c5e81737adb15f5b255e39603d08
breabang <breanna.bang@gmail.com>
2022-08-12 16:33:39 -0600

    updated project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'Views/index.ejs')
+    // Note: __dirname is the current directory you're in. Try logging it and see what you get!
+    // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+  })
+app.listen(8000, function() {
+    console.log('listening on 8000')
+    
+  })
+ 
