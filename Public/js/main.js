3a0e6b899950daafee3cee657b05f4bbbf924e7c
breabang <breanna.bang@gmail.com>
2022-08-13 13:41:58 -0600

    update

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,5 +1,5 @@
 const deleteBtn = document.querySelectorAll('.fa-trash')
-const item = document.querySelectorAll('.item span')
+const item = document.querySelectorAll('.item span') 
 const itemCompleted = document.querySelectorAll('.item span.completed')
 
 Array.from(deleteBtn).forEach((element)=>{
